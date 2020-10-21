c759e7e8d09acfa8661e5d077f4822e0520f209c
Moaty <mostafaamoaty@gmail.com>
2020-10-21 21:42:11 +0200

    Coming soon page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/cratena-logo.png";
+import { motion } from "framer-motion";
+import "./App.css";
 
+const bounceTransition = {
+  y: {
+    duration: 2,
+    yoyo: Infinity,
+    ease: "easeInOut",
+  },
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <motion.div animate={{ y: -100 }} transition={bounceTransition}>
         <img src={logo} className="App-logo" alt="logo" />
+      </motion.div>
+      <div className="footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>
+            developed by <a href="http://moaty.me">Moaty</a>
+          </code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
